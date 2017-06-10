47b0e3e3aebe7a847b8ad769183c13a8c5eb6c5d
Julian Mundhahs <julian.mundhahs@gmail.com>
2017-06-10 11:21:03 +0200

    Option to open all Activities from the starting Activity

--- app/src/main/java/org/jugendhackt/camera_warner/MainActivity.java
+++ app/src/main/java/org/jugendhackt/camera_warner/MainActivity.java
@@ -3,6 +3,7 @@ package org.jugendhackt.camera_warner;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +12,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+    }
+
+    public void openMap(View view) {
         Intent mapsActivity = new Intent(this, MapsActivity.class);
+        startActivity(mapsActivity);
+    }
+
+    public void openSettings(View view) {
+        Intent mapsActivity = new Intent(this, EinstellungenActivity.class);
         startActivity(mapsActivity);
     }
 }
