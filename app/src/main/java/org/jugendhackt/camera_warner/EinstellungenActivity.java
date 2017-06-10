78d3d9ae5fc0976ecdc177bec80481e8c73b6f94
xCubeArrow <martinharald.goetze@gmail.com>
2017-06-10 12:21:25 +0200

    EinstellungenActivity & LoginActivity
    Added Login Activity

--- app/src/main/java/org/jugendhackt/camera_warner/EinstellungenActivity.java
+++ app/src/main/java/org/jugendhackt/camera_warner/EinstellungenActivity.java
@@ -4,10 +4,12 @@ package org.jugendhackt.camera_warner;
  * Created by Martin Goetze on 10.06.2017.
  */
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.Preference;
+
 import android.preference.PreferenceActivity;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.widget.Toast;
 
@@ -17,6 +19,16 @@ public class EinstellungenActivity extends PreferenceFragmentCompat
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Preference Button = findPreference("pref_anmelden");
+        Button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+
+                Intent intent = new Intent(getContext(), LoginActivity.class);
+                startActivity(intent);
+                return true;
+            }
+        });
     }
 
     @Override
@@ -30,4 +42,5 @@ public class EinstellungenActivity extends PreferenceFragmentCompat
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         return false;
     }
+
 }
