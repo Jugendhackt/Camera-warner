aae9b02323e8d57d8c9b2bf4feb805317073d46f
Julian Mundhahs <julian.mundhahs@gmail.com>
2017-06-10 22:09:06 +0200

    Set fitting summary and catch not allowed values in fields

--- app/src/main/java/org/jugendhackt/camera_warner/SettingsFragment.java
+++ app/src/main/java/org/jugendhackt/camera_warner/SettingsFragment.java
@@ -9,17 +9,31 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import android.preference.PreferenceActivity;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.EditTextPreference;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
 import android.widget.Toast;
 
 public class SettingsFragment extends PreferenceFragmentCompat
-        implements Preference.OnPreferenceChangeListener {
+        implements Preference.OnPreferenceChangeListener, SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Preference Button = findPreference("pref_anmelden");
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+
+        // Add visualizer preferences, defined in the XML file in res->xml->pref_visualizer
+        addPreferencesFromResource(R.xml.preferences);
+
+        Preference Button = findPreference(getString(R.string.pref_login_key));
         Button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
@@ -29,18 +43,94 @@ public class SettingsFragment extends PreferenceFragmentCompat
                 return true;
             }
         });
+
+
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        int count = prefScreen.getPreferenceCount();
+
+        for (int i = 0; i < count; i++) {
+            Preference p = prefScreen.getPreference(i);
+            // You don't need to set up preference summaries for checkbox preferences because
+            // they are already set up in xml using summaryOff and summary On
+            if (!(p instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(p.getKey(), "");
+                setPreferenceSummary(p, value);
+            }
+        }
+
+        Preference preference = findPreference(getString(R.string.pref_radius_key));
+        preference.setOnPreferenceChangeListener(this);
     }
 
     @Override
-    public void onCreatePreferences(Bundle bundle, String s) {
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        if (null != preference) {
+            // Updates the summary for the preference
+            if (!(preference instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(preference.getKey(), "");
+                setPreferenceSummary(preference, value);
+            }
+        }
+    }
 
-        // Add visualizer preferences, defined in the XML file in res->xml->pref_visualizer
-        addPreferencesFromResource(R.xml.preferences);
+    /**
+     * Updates the summary for the preference
+     *
+     * @param preference The preference to be updated
+     * @param value      The value that the preference was updated to
+     */
+    private void setPreferenceSummary(Preference preference, String value) {
+        if (preference instanceof ListPreference) {
+            // For list preferences, figure out the label of the selected value
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(value);
+            if (prefIndex >= 0) {
+                // Set the summary to that label
+                listPreference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else if (preference instanceof EditTextPreference) {
+            if (preference.getKey().equals(getString(R.string.pref_radius_key))) {
+                preference.setSummary(value + "m");
+            } else {
+                // For EditTextPreferences, set the summary to the value's simple string representation.
+                preference.setSummary(value);
+            }
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
+        // In this context, we're using the onPreferenceChange listener for checking whether the
+        // size setting was set to a valid value.
+
+        Toast error = Toast.makeText(getContext(), "Please select a number greater than 0", Toast.LENGTH_SHORT);
+
+        // Double check that the preference is the size preference
+        String sizeKey = getString(R.string.pref_radius_key);
+        if (preference.getKey().equals(sizeKey)) {
+            String stringSize = (String) newValue;
+            try {
+                double size = Double.parseDouble(stringSize);
+                // If the number is outside of the acceptable range, show an error.
+                if (size <= 0) {
+                    error.show();
+                    return false;
+                }
+            } catch (NumberFormatException nfe) {
+                // If whatever the user entered can't be parsed to a number, show an error
+                error.show();
+                return false;
+            }
+        }
+        return true;
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
 }
