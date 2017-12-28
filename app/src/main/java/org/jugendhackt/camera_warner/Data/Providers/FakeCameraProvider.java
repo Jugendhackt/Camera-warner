61f2198b734e153c8a1cb2793bcbba4221e1ad57
Julian Mundhahs <julian.mundhahs@gmail.com>
2017-12-28 18:43:54 +0100

    Update FakeCameraProvider to new interface

--- app/src/main/java/org/jugendhackt/camera_warner/Data/Providers/FakeCameraProvider.java
+++ app/src/main/java/org/jugendhackt/camera_warner/Data/Providers/FakeCameraProvider.java
@@ -1,9 +1,11 @@
 package org.jugendhackt.camera_warner.Data.Providers;
 
+import android.location.Location;
+
 import org.jugendhackt.camera_warner.Data.Model.Camera;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 
 /**
@@ -13,8 +15,8 @@ import java.util.List;
 public class FakeCameraProvider extends AbstractDataProvider {
 
     @Override
-    protected List<Camera> forceFetch() {
-        List<Camera> cameras = new LinkedList<>();
+    protected Set<Camera> loadData(Location location) {
+        Set<Camera> cameras = new LinkedHashSet<>();
         cameras.add(new Camera(-31, 142));
         cameras.add(new Camera(53.563, 9.971));
         cameras.add(new Camera(0, 0));
